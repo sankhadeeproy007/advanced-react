e579aeed8e5a88899aeec981223721703a073364
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2019-11-30 12:51:02 +0530

    chore(): changes

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hey!</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = () => {
+  return (
+    <div>
+      <h1>Sell!</h1>
+    </div>
+  );
+};
+
+export default Sell;
